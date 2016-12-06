eaa11e0b9fe4b59ef6cd915d275131ed6627d753
Jenne <jenne@bilbie-alexander.com>
2016-12-06 00:07:48 -0800

    Day 06

---
+++ day06_part1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 16:31:53 2016
+
+@author: jbilbie
+
+Advent of Code 2016
+--- Day 6: Signals and Noise ---
+"""
+
+f = open('day06_input.txt', 'r')
+
+letterCountDict = {}
+correctMessage = ''
+
+for line in f:
+    for i in range(len(line)-1):
+        if not i in letterCountDict.keys():
+            letterCountDict[i] = {line[i]: 0}
+        if not line[i] in letterCountDict[i]:
+            letterCountDict[i][line[i]] = 0
+        letterCountDict[i][line[i]] += 1
+
+for key in letterCountDict:
+    sortedByValue = sorted(letterCountDict[key].items(), key=lambda x: (-x[1]))
+    correctMessage += sortedByValue[0][0]
+
+print('The error-corrected message is {}.'.format(correctMessage))     
